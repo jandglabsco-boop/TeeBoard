5f8e6636d681c05198801f575bbe4adc2d61dd70
jandglabsco-boop <jandglabsco@gmail.com>
2026-07-25 17:45:50 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // - SUPABASE_URL looks like https://xxxxxxxx.supabase.co
 // - SUPABASE_ANON_KEY is the long "anon public" key (safe to expose in client code)
 window.TEEBOARD_CONFIG = {
-  SUPABASE_URL: "YOUR_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_URL: "https://yfvglcqqvuywdpvynjqn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmdmdsY3FxdnV5d2RwdnluanFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwMDkyMzIsImV4cCI6MjEwMDU4NTIzMn0.Osf04Z7u3ESMa7QbBAv1VG9b5WaBmT_6noXpKyU19hI",
 };
